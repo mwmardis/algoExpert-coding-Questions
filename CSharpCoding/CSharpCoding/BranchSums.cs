55fe25901de20e1da6334767084441984e62c072
mwmardis <mwmardis@gmail.com>
2021-12-22 13:23:27 -0600

    BranchSums DFS

---
+++ CSharpCoding/CSharpCoding/BranchSums.cs
@@ -0,0 +1,48 @@
+﻿namespace CSharpCoding
+{
+    public static class BranchSums
+    {
+        public static List<int> BranchSumsSolution(BinaryTree? root)
+        {
+            var returnList = new List<int>();
+            BranchSumsSolutionHelper(root, 0, returnList);
+
+            return returnList;
+        }
+
+        public static void BranchSumsSolutionHelper(BinaryTree? node, int runningSum, List<int> sums)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            var newRunningSum = runningSum + node.value;
+
+            if (node.left == null && node.right == null)
+            {
+                sums.Add(newRunningSum);
+                return;
+            }
+
+            BranchSumsSolutionHelper(node.left, newRunningSum, sums);
+            BranchSumsSolutionHelper(node.right, newRunningSum, sums);
+
+        }
+    }
+
+    public class BinaryTree
+    {
+        public int value;
+        public BinaryTree? left;
+        public BinaryTree? right;
+
+        public BinaryTree(int value)
+        {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    
+}
