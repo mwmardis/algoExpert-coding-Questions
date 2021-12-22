491d87342d6d748f0143d8b7e01b26d80ed4cb4c
mwmardis <mwmardis@gmail.com>
2021-12-22 12:30:04 -0600

    NonConstructiveChange completed

---
+++ CSharpCoding/CSharpCoding/NonConstructibleChange.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharpCoding
+{
+    public static class NonConstructibleChange
+    {
+        public static int NonConstructiveChange(int[] coins)
+        {
+            var leastAmount = 0;
+            if (!coins.Any())
+            {
+                return 1;
+            }
+
+            if (coins.Length == 1 && coins[0] != 1)
+            {
+                return 1;
+            }
+
+            if (coins.Length == 1 && coins[0] == 1)
+            {
+                return 2;
+            }
+
+
+            foreach (var coin in coins.OrderBy(x => x))
+            {
+                if (coin > leastAmount + 1 && leastAmount != 0)
+                {
+                    return leastAmount + 1;
+                }
+
+                leastAmount += coin;
+            }
+
+            return leastAmount + 1;
+        }
+    }
+}
---
+++ CSharpCoding/CSharpCoding/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CSharpCoding;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(NonConstructibleChange.NonConstructiveChange(new[] {1, 3, 5}));
