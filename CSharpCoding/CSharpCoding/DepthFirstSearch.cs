3c90391520a89cfc21027d399eee5b8ab3e8d154
mwmardis <mwmardis@gmail.com>
2022-01-26 18:54:05 -0600

    DFS

---
+++ CSharpCoding/CSharpCoding/DepthFirstSearch.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpCoding
+{
+    public static class DepthFirstSearch
+    {
+        // Time = O(V + E)
+        // Space = O(V)
+        public class Node
+        {
+            public string name;
+            public List<Node> children = new List<Node>();
+
+            public Node(string name)
+            {
+                this.name = name;
+            }
+
+            public List<string> DepthFirstSearch(List<string> array)
+            {
+                array.Add(name);
+                foreach (var node in children)
+                {
+                    node.DepthFirstSearch(array);
+                }
+                return array;
+            }
+
+            public Node AddChild(string name)
+            {
+                Node child = new Node(name);
+                children.Add(child);
+                return this;
+            }
+        }
+    }
+}
